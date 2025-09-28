5818c55ecf1a97bc3210ee1cc2ea32d390950c3e
NoranBESTEAM <nmohammed@be-steam.com>
2025-09-28 15:47:09 +0300

    Route & Controller Dashboard

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [
+            __DIR__.'/../routes/web.php',
+            __DIR__.'/../routes/frontend.php', // frontend routes
+            __DIR__.'/../routes/backend.php',  // backend routes
+        ],
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
