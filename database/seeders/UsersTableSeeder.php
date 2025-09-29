f78074d2195e4674f45d9fac854fe1330e649ac0
NoranBESTEAM <nmohammed@be-steam.com>
2025-09-29 18:54:41 +0300

    Install Laratrust Package

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'super admin',
+            'email' => 'super_admin@app.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $user->addRole('super_admin');
+    }
+}
